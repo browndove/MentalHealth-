2fb92472ca78a4130bf370ea2e567191126b9112
Chris Ampeh <bluebird23szn@gmail.com>
2025-07-12 21:27:46 +0000

    no restore the old content in the .env

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,10 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [
+    googleAI({
+      apiKey: 'AIzaSyD5u39fF5iMYUT8EALc7QsPAi04KYSK6Rg',
+    }),
+  ],
   model: 'googleai/gemini-2.0-flash',
 });
